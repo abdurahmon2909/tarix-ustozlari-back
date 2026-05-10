6c89e5800dc1107da0e052c7d10e806f3c719a4b
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-10 13:26:58 +0500

    add book model

--- app/models/book.py
+++ app/models/book.py
@@ -1,14 +1,10 @@
-from datetime import datetime
-
-from sqlalchemy import DateTime
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Text
-
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
-from app.core.database import Base
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.models.base import Base
 
 
 class Book(Base):
@@ -16,35 +12,14 @@ class Book(Base):
 
     id: Mapped[int] = mapped_column(
         Integer,
-        primary_key=True
+        primary_key=True,
+        index=True
     )
 
     title: Mapped[str] = mapped_column(
-        String,
-        nullable=False
+        String(255)
     )
 
     subject: Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
-
-    class_name: Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
-
-    cover_image: Mapped[str | None] = mapped_column(
-        String,
-        nullable=True
-    )
-
-    description: Mapped[str | None] = mapped_column(
-        Text,
-        nullable=True
-    )
-
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime,
-        default=datetime.utcnow
+        String(255)
     )
